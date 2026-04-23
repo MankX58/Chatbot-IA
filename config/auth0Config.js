85588841566f9f64cb334925dcb5b2b3fbccf554
Manuel <manuelguvi500@gmail.com>
2026-04-23 01:25:51 -0500

    asdasd

--- config/auth0Config.js
+++ config/auth0Config.js
@@ -2,5 +2,5 @@
 export const auth0Config = {
   domain: import.meta.env.VITE_AUTH0_DOMAIN || "tu-dominio.auth0.com",
   clientId: import.meta.env.VITE_AUTH0_CLIENT_ID || "tu-client-id",
-  redirectUri: window.location.origin + '/home'
+  redirectUri: window.location.origin
 };
